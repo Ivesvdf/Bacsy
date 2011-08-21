f84fffce56f14903c5b30cf3902703b64103e5c7
Nathan Samson <nathansamson@gmail.com>
2011-08-21 22:01:19 +0200

    Adapt FindPoco module so it uses COMPONENTS

--- cmake_modules/FindPoco.cmake
+++ cmake_modules/FindPoco.cmake
@@ -1,29 +1,21 @@
-set(SEARCHPATH ${CMAKE_SOURCE_DIR}/external/ /usr /usr/local /opt /opt/local $ENV{HOME}/opt)
-#			
 find_path(POCO_INCLUDE_DIRS    Poco/
-              PATHS            ${SEARCHPATH}
               PATH_SUFFIXES    include
 )
 
-set(POCO_COMPONENTS
-        PocoNet
-        PocoUtil
-        PocoFoundation
+find_library(POCO_FOUNDATION_LIB     PocoFoundation
+                 PATH_SUFFIXES       lib
 )
 
-set(POCO_LIBRARIES "")
-
-foreach(COMPONENT ${POCO_COMPONENTS})
-	find_library(${COMPONENT}_LIB     ${COMPONENT}
-	                 PATHS            ${SEARCHPATH}
-	                 PATH_SUFFIXES    lib
+foreach(COMPONENT ${Poco_FIND_COMPONENTS})
+	find_library(POCO_${COMPONENT}_LIB     "Poco${COMPONENT}"
+	                 PATH_SUFFIXES         lib
 	)
 
-	list(APPEND POCO_LIBRARIES ${${COMPONENT}_LIB})
-	list(REMOVE_DUPLICATES POCO_LIBRARIES)
+	list(APPEND POCO_LIBRARIES ${POCO_${COMPONENT}_LIB})
 endforeach()
 
-unset(SEARCHPATH)
+list(APPEND POCO_LIBRARIES ${POCO_FOUNDATION_LIB})
+list(REMOVE_DUPLICATES POCO_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Poco DEFAULT_MSG
